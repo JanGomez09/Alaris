6e606a575bf40a70012260940baa4bb430e8787a
Your Name <you@example.com>
2026-05-25 10:03:00 -0600

    contactos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
 
-    id("com.android.application")
 
-    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -52,5 +51,4 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
 }
